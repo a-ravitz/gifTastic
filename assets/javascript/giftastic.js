1d1e5ee9aefceec64b2f7d34714f3da7d94b3cc5
ravitz <adam.ravitz@gmail.com>
2019-03-18 23:51:08 -0400

    march 18.0

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,55 +1,122 @@
-var cats = ["Monty", "Honey Bee", "Venus", "Lil Bub", "Maru", "Grumpy cat", "Garfi", "Shironeko", "Snoopy", "Colonel Meow"];
+var topics = ["marge simpson", "bart simpson", "homer simpson", "lisa simpson", "ned flanders", "mr. burns"];
+var favorites = []; 
+var divButtons = $("<div>")
+  .addClass("col-md-12")
+  .attr("id", "button");
+$("body").append(divButtons);
 
-function alertCat () {
-    var catName = $(this).attr("data-cat");
-    console.log(catName);
-}
+function makeButtons() {
 
-function renderButtons () {
+  $("#button").empty();
 
-    $("#buttons").empty();
+  for (var i = 0; i < topics.length; i++) {
 
-    for (var i = 0; i < cats.length; i++) {
     var catButtons = $("<button>")
-        catButtons 
-            .text(cats[i])
-            .addClass("cat")
-            .attr("id", "catButton")
-            .attr("data-cat", cats[i]);
-            $("#buttons").append(catButtons);
-    }
-} 
+    catButtons
+      .attr("id", "catButton")
+      .attr("data-cat", topics[i])
+      .addClass("cats btn btn-primary m-2")
+      .text(topics[i]);
+    $("#button").append(catButtons);
+  }
+} makeButtons();
+var divRow = $("<div>")
+    divRow 
+      .addClass("row")
+    $("body").append(divRow)
 
-$("#add-cat").on("click", function(event) {
-        event.preventDefault();
+var divContainer = $("<div>")
+    divContainer  
+      .addClass("col-md-12")
+    $(divRow).append(divContainer)
 
-        var newCat = $("#cat-input").val();
-        cats.push(newCat);
-        renderButtons()
-        console.log(newCat);
-        console.log(cats)
-       
+var divForm = $("<form>")
+  .attr("id", "form")
+  .addClass("card float-right p-3 m-3")
+  .append('<label for="stuff-input">Add another simpsons character!</label>')
+  .append('<input type="text" id="form-input"><br>')
+  .append('<input class="btn btn-primary m-2" id="add-stuff" type="submit" value="more cats!">')
+  $(divContainer).append(divForm)
+
+$("#add-stuff").on("click", function (event) {
+  event.preventDefault();
+  var newCat = $("#form-input").val().trim();
+  topics.push(newCat);
+  makeButtons()
+
 });
 
-$(document).on("click", ".cat", alertCat);
-// Calling the renderButtons function to display the intial buttons
-renderButtons();
+$(document).on("click", "#catButton", function (event) {
+  event.preventDefault();
 
-console.log(cats);
-//     var apiKey = "HxwynQp8ob7R6ae0v86N9VgR8hhNS0DE"
-//     var cat = $(this).attr("data-cat");
-//     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cat + "&api_key=" + apiKey +"&limit=5"
+  var apiKey = "HxwynQp8ob7R6ae0v86N9VgR8hhNS0DE";
+  var cat = $(this).attr("data-cat");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cat + "&api_key=" + apiKey + "&limit=10"
 
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
-//       $("body").append("img", "src=" + response.images.fixed_height_still.url);
-//       console.log(response.images.fixed_height_still.url)
-//     });
-  
-// }); 
+  alert(cat);
 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
 
+  }).then(function (response) {
 
-//apikey 
+    var results = response.data
+    console.log(results);
+
+    function images() {
+      for (var j = 0; j < results.length; j++) {
+
+        var gifDiv = $("<div>");
+            gifDiv 
+              .addClass("col-md-9")
+              $(divContainer).append(gifDiv);
+        var gifCard = $("<div>")
+              .addClass("float-left m-1")
+        var rating = results[j].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var topicImage = $("<img>");
+            topicImage
+              .attr("data-state", "still")
+              .attr("data-still", results[j].images.fixed_height_still.url)
+              .attr("data-animate", results[j].images.fixed_height.url)
+              .attr("src", results[j].images.fixed_height_still.url)
+              .addClass("img-responsive")
+              .addClass("gif");
+        gifCard.append(p);
+        gifCard.append(topicImage);
+        $(gifDiv).prepend(gifCard);
+        console.log(p)
+        var downloadButton = $("<button>")
+            downloadButton
+              .attr("id", "dwnBtn")
+              .attr("href", results[j].images.original.url)
+              .text("download!")
+        p.append(downloadButton)
+
+        // jQuery(document).ready(function($) {
+
+       $('downloadButton[href$=".gif"]')
+          .attr('download', '')
+          .attr('target', '_blank'); 
+        // });
+      } 
+
+    } images(results)
+
+        $(".gif").on("click", function (event) {
+
+          var state = $(this).attr("data-state");
+
+          console.log(state);
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+  });
+
+});
